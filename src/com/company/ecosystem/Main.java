1bc7ff066f79ae46a89d05405b3907d993d11ca6
jannopet <janno.peterson@gmail.com>
2018-01-09 00:27:04 +0200

    UI functionality. Changing Food spawning rate now works.

--- src/com/company/ecosystem/Main.java
+++ src/com/company/ecosystem/Main.java
@@ -2,12 +2,17 @@ package com.company.ecosystem;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 import java.util.*;
 import java.util.Timer;
 
 public class Main extends JPanel{
+    private static final int CONTROL_PANEL_WIDTH = 250;
+    private static final int CONTROL_PANEL_HEIGHT = 1000;
     private static final int WORLD_WIDTH = 1400;
     private static final int WORLD_HEIGHT = 1000;
+    private static final int SCREEN_WIDTH = 1650;
+    private static final int SCREEN_HEIGHT = 1000;
     private static final int FRAMES_PER_SECOND = 30;
     private static final int MS_TO_WAIT = 1000 / FRAMES_PER_SECOND;
     private static final int STARTING_BASIC_FOOD_COUNT = 30;
@@ -22,14 +27,64 @@ public class Main extends JPanel{
     private static final Color ALTERNATE_FOOD_COLOR = Color.GRAY;
     private static final Color PREY_COLOR = Color.BLUE;
     private static final Color PREDATOR_COLOR = Color.RED;
+    private static final String BASIC_FOOD_STR = "Basic food";
+    private static final String ALT_FOOD_STR = "Alternate food";
+    private static final String PREDATOR_STR = "Predators";
+    private static final String PREY_STR = "Prey animals";
+    private static final String UPDATE_ACTION_COMMAND = "Update";
+    private static final String RESET_DEFAULTS_ACTION_COMMAND = "Defaults";
+    private static final double PREDATOR_REPRODUCTION_RATE = 0.0025;
+    private static final double PREY_REPRODUCTION_RATE = 0.004;
+    private static final double PREDATOR_MUTATION_RATE = 0.15;
+    private static final double PREY_MUTATION_RATE = 0.15;
+    private static final Dimension inputDimension = new Dimension(45,20);
 
     private Timer animationTimer;
     private TimerTask animationTask;
+    private TimerTask updateTask;
     private ArrayList<Prey> preyList;
     private ArrayList<BasicFood> basicFoodList;
     private ArrayList<AlternateFood> alternateFoodList;
     private ArrayList<Predator> predatorList;
     private Random ran;
+
+    private JPanel controlPanel;
+    private JPanel countPanel;
+    private JLabel basicFoodCount;
+    private JLabel altFoodCount;
+    private JLabel predatorCount;
+    private JLabel preyCount;
+
+    private JPanel inputPanel;
+    private JPanel basicFoodSpawnPanel;
+    private JPanel altFoodSpawnPanel;
+    private JPanel predatorReprRatePanel;
+    private JPanel preyReprRatePanel;
+    private JPanel predatorMutationRatePanel;
+    private JPanel preyMutationRatePanel;
+    private JTextField basicFoodSpawnInput;
+    private JTextField altFoodSpawnInput;
+    private JTextField predatorReprRateInput;
+    private JTextField preyReprRateInput;
+    private JTextField predatorMutationRateInput;
+    private JTextField preyMutationRateInput;
+    private JLabel basicFoodSpawnInputLabel;
+    private JLabel altFoodSpawnInputLabel;
+    private JLabel predatorReprRateInputLabel;
+    private JLabel preyReprRateInputLabel;
+    private JLabel predatorMutationRateInputLabel;
+    private JLabel preyMutationRateInputLabel;
+
+    private JButton updateButton;
+    private JButton defaultButton;
+
+    private double currentBasicFoodSpawnRate;
+    private double currentAltFoodSpawnRate;
+    private double currentPredatorReproductionRate;
+    private double currentPreyReproductionRate;
+    private double currentPredatorMutationRate;
+    private double currentPreyMutationRate;
+
 
     public Main() {
         ran = new Random();
@@ -37,13 +92,25 @@ public class Main extends JPanel{
         basicFoodList = new ArrayList<>();
         alternateFoodList = new ArrayList<>();
         predatorList = new ArrayList<>();
+
+        currentBasicFoodSpawnRate = BASIC_FOOD_SPAWN_RATE;
+        currentAltFoodSpawnRate = ALTERNATE_FOOD_SPAWN_RATE;
+        currentPredatorReproductionRate = PREDATOR_REPRODUCTION_RATE;
+        currentPreyReproductionRate = PREY_REPRODUCTION_RATE;
+        currentPredatorMutationRate = PREDATOR_MUTATION_RATE;
+        currentPreyMutationRate = PREY_MUTATION_RATE;
+
         initializeFood();
         initializePrey();
         initializePredators();
+        initializeControlPanel();
 
         animationTimer = new Timer();
         animationTask = new AnimationUpdater();
+        updateTask = new InfoUpdater();
     }
+
+
 
     private void initializeFood() {
         for (int i = 0; i < STARTING_BASIC_FOOD_COUNT; i++) {
@@ -74,8 +141,100 @@ public class Main extends JPanel{
         }
     }
 
+    private void initializeControlPanel(){
+        this.setLayout(new BorderLayout());
+        controlPanel = new JPanel();
+        controlPanel.setPreferredSize(new Dimension(CONTROL_PANEL_WIDTH, CONTROL_PANEL_HEIGHT));
+        controlPanel.setLayout(new BoxLayout(controlPanel, BoxLayout.Y_AXIS));
+
+        countPanel = new JPanel();
+        countPanel.setAlignmentX(LEFT_ALIGNMENT);
+        countPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+        countPanel.setMaximumSize(new Dimension(150, 90));
+
+        basicFoodCount = new JLabel(BASIC_FOOD_STR + ": " + Integer.toString(basicFoodList.size()));
+        altFoodCount = new JLabel(ALT_FOOD_STR + ": " + Integer.toString(alternateFoodList.size()));
+        predatorCount = new JLabel(PREDATOR_STR + ": " + Integer.toString(predatorList.size()));
+        preyCount = new JLabel(PREY_STR + ": " + Integer.toString(preyList.size()));
+
+        countPanel.add(basicFoodCount);
+        countPanel.add(altFoodCount);
+        countPanel.add(predatorCount);
+        countPanel.add(preyCount);
+
+        inputPanel = new JPanel();
+        inputPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
+        inputPanel.setAlignmentX(LEFT_ALIGNMENT);
+        inputPanel.setMaximumSize(new Dimension(250, 300));
+
+        basicFoodSpawnPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        basicFoodSpawnInput = new JTextField(Double.toString(BASIC_FOOD_SPAWN_RATE));
+        basicFoodSpawnInput.setPreferredSize(inputDimension);
+        basicFoodSpawnInputLabel = new JLabel("Basic Food Spawn Rate: ");
+        basicFoodSpawnPanel.add(basicFoodSpawnInputLabel);
+        basicFoodSpawnPanel.add(basicFoodSpawnInput);
+
+        altFoodSpawnPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        altFoodSpawnInput = new JTextField(Double.toString(ALTERNATE_FOOD_SPAWN_RATE));
+        altFoodSpawnInput.setPreferredSize(inputDimension);
+        altFoodSpawnInputLabel = new JLabel("Alternate Food Spawn Rate: ");
+        altFoodSpawnPanel.add(altFoodSpawnInputLabel);
+        altFoodSpawnPanel.add(altFoodSpawnInput);
+
+        predatorReprRatePanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+        predatorReprRateInput = new JTextField(Double.toString(PREDATOR_REPRODUCTION_RATE));
+        predatorReprRateInput.setPreferredSize(inputDimension);
+        predatorReprRateInputLabel = new JLabel("Predator Reproduction Rate: ");
+        predatorReprRatePanel.add(predatorReprRateInputLabel);
+        predatorReprRatePanel.add(predatorReprRateInput);
+
+        preyReprRatePanel = new JPanel(new FlowLayout());
+        preyReprRateInput = new JTextField(Double.toString(PREY_REPRODUCTION_RATE));
+        preyReprRateInput.setPreferredSize(inputDimension);
+        preyReprRateInputLabel = new JLabel("Prey Reproduction Rate: ");
+        preyReprRatePanel.add(preyReprRateInputLabel);
+        preyReprRatePanel.add(preyReprRateInput);
+
+        predatorMutationRatePanel = new JPanel(new FlowLayout());
+        predatorMutationRateInput = new JTextField(Double.toString(PREDATOR_MUTATION_RATE));
+        predatorMutationRateInput.setPreferredSize(inputDimension);
+        predatorMutationRateInputLabel = new JLabel("Predator Mutation Rate: ");
+        predatorMutationRatePanel.add(predatorMutationRateInputLabel);
+        predatorMutationRatePanel.add(predatorMutationRateInput);
+
+        preyMutationRatePanel = new JPanel(new FlowLayout());
+        preyMutationRateInput = new JTextField(Double.toString(PREY_MUTATION_RATE));
+        preyMutationRateInput.setPreferredSize(inputDimension);
+        preyMutationRateInputLabel = new JLabel("Prey Mutation Rate: ");
+        preyMutationRatePanel.add(preyMutationRateInputLabel);
+        preyMutationRatePanel.add(preyMutationRateInput);
+
+        ButtonClickListener listener = new ButtonClickListener();
+        updateButton = new JButton("Update parameters");
+        updateButton.setActionCommand(UPDATE_ACTION_COMMAND);
+        updateButton.addActionListener(listener);
+        defaultButton = new JButton("Reset to defaults");
+        defaultButton.setActionCommand(RESET_DEFAULTS_ACTION_COMMAND);
+        defaultButton.addActionListener(listener);
+
+        inputPanel.add(basicFoodSpawnPanel);
+        inputPanel.add(altFoodSpawnPanel);
+        inputPanel.add(predatorReprRatePanel);
+        inputPanel.add(preyReprRatePanel);
+        inputPanel.add(predatorMutationRatePanel);
+        inputPanel.add(preyMutationRatePanel);
+        inputPanel.add(updateButton);
+        inputPanel.add(defaultButton);
+
+        controlPanel.add(countPanel);
+        controlPanel.add(inputPanel);
+
+        this.add(controlPanel, BorderLayout.LINE_END);
+    }
+
     private void start() {
         animationTimer.schedule(animationTask, 0, MS_TO_WAIT);
+        animationTimer.schedule(updateTask, 0, MS_TO_WAIT);
     }
 
     @Override
@@ -122,6 +281,8 @@ public class Main extends JPanel{
         JFrame frame = new JFrame();
         Main panel = new Main();
         frame.getContentPane().add(panel);
+        frame.setPreferredSize(new Dimension(SCREEN_WIDTH, SCREEN_HEIGHT));
+
         frame.pack();
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -168,14 +329,65 @@ public class Main extends JPanel{
             predatorList.addAll(bornPredatorList);
 
             double randomDouble = ran.nextDouble();
-            if (randomDouble < BASIC_FOOD_SPAWN_RATE) {
+            if (randomDouble < currentBasicFoodSpawnRate) {
                 basicFoodList.add(new BasicFood(WORLD_WIDTH, WORLD_HEIGHT, BASIC_FOOD_RADIUS));
             }
-            if (randomDouble < ALTERNATE_FOOD_SPAWN_RATE) {
+            if (randomDouble < currentAltFoodSpawnRate) {
                 alternateFoodList.add(new AlternateFood(WORLD_WIDTH, WORLD_HEIGHT, ALTERNATE_FOOD_RADIUS));
             }
 
             repaint();
         }
     }
+
+    private class InfoUpdater extends TimerTask {
+        @Override
+        public void run(){
+            basicFoodCount.setText(BASIC_FOOD_STR + ": " + Integer.toString(basicFoodList.size()));
+            altFoodCount.setText(ALT_FOOD_STR + ": " + Integer.toString(alternateFoodList.size()));
+            predatorCount.setText(PREDATOR_STR + ": " + Integer.toString(predatorList.size()));
+            preyCount.setText(PREY_STR + ": " + Integer.toString(preyList.size()));
+        }
+    }
+
+
+    private class ButtonClickListener implements ActionListener{
+        public Boolean validInput(){
+            double basicSpawn;
+            double altSpawn;
+
+            try{
+                basicSpawn = Double.parseDouble(basicFoodSpawnInput.getText());
+                altSpawn = Double.parseDouble(altFoodSpawnInput.getText());
+            } catch(Exception e){
+                return false;
+            }
+
+            if(1 < basicSpawn || basicSpawn < 0){
+                return false;
+            }
+            if(1 < altSpawn || altSpawn < 0){
+                return false;
+            }
+            System.out.println("Valid input.");
+            return true;
+        }
+
+        public void actionPerformed(ActionEvent e) {
+            if(e.getActionCommand().equals(UPDATE_ACTION_COMMAND)){
+                if(validInput()){
+                    currentBasicFoodSpawnRate = Double.parseDouble(basicFoodSpawnInput.getText());
+                    currentAltFoodSpawnRate = Double.parseDouble(altFoodSpawnInput.getText());
+                    System.out.println("Changed parameters.");
+                }
+            } else if(e.getActionCommand().equals(RESET_DEFAULTS_ACTION_COMMAND)){
+                currentBasicFoodSpawnRate = BASIC_FOOD_SPAWN_RATE;
+                currentAltFoodSpawnRate = ALTERNATE_FOOD_SPAWN_RATE;
+                basicFoodSpawnInput.setText(Double.toString(currentBasicFoodSpawnRate));
+                altFoodSpawnInput.setText(Double.toString(currentAltFoodSpawnRate));
+                System.out.println("Reset parameters");
+            }
+        }
+    }
+
 }
